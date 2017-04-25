6fff2f9b284bc0920c844e1f67e823f5be52ca85
Daniel Fuentes <daniel.fuentes@segment.com>
2017-04-25 13:06:50 -0700

    Add config flags, support tcp/udp/unixgram servers

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,30 +9,54 @@ import (
 
 	"net/http"
 	_ "net/http/pprof"
+	"net/url"
 
+	"github.com/segmentio/conf"
+	"github.com/segmentio/kit/log"
 	"github.com/segmentio/rate-limiting-log-proxy/container"
 	"github.com/segmentio/rate-limiting-log-proxy/logger"
 	syslog "gopkg.in/mcuadros/go-syslog.v2"
 )
 
+type config struct {
+	DockerHost        string        `conf:"d" help:"Docker host to connect to"`
+	RateLimitInterval time.Duration `conf:"i" help:"Rate limit interval"`
+	RateLimitBurst    int           `conf:"b" help:"Rate limit burst"`
+	ProfilingPort     string        `conf:"p" help:"Port to host profiling endpoint"`
+	SyslogAddress     string        `conf:"s" help:"Address to bind syslog server to (ex. udp://0.0.0.0:514)"`
+}
+
+//
+var DefaultConfig = config{
+	DockerHost:        "unix:///var/run/docker.sock",
+	RateLimitInterval: 5 * time.Second,
+	RateLimitBurst:    500,
+	ProfilingPort:     "6060",
+	SyslogAddress:     "unixgram:///var/run/rate-limiting-log-proxy.sock",
+}
+
 func main() {
-	// Start http server for pprof
+	conf.Load(&DefaultConfig)
+
 	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
+		profilingAddress := fmt.Sprintf("localhost:%s", DefaultConfig.ProfilingPort)
+		log.Infof("Starting profiling server at %s...", profilingAddress)
+		http.ListenAndServe(profilingAddress, nil)
 	}()
 
-	// Start syslog server
 	server := syslog.NewServer()
 	server.SetFormat(syslog.RFC3164)
-	server.ListenUDP("0.0.0.0:10514")
+	if err := setupListener(server, DefaultConfig.SyslogAddress); err != nil {
+		log.Fatal(err)
+	}
 
-	containerLookupService, err := container.NewDockerLookup("unix:///var/run/docker.sock")
+	containerLookupService, err := container.NewDockerLookup(DefaultConfig.DockerHost)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	loggerFactory := logger.NewLoggerFactory(logger.Journald)
-	handler := NewRateLimitingHandler(5*time.Second, 300, containerLookupService, loggerFactory)
+	handler := NewRateLimitingHandler(DefaultConfig.RateLimitInterval, DefaultConfig.RateLimitBurst, containerLookupService, loggerFactory)
 	server.SetHandler(handler)
 
 	server.Boot()
@@ -41,8 +65,33 @@ func main() {
 	signal.Notify(sigchan, os.Interrupt, syscall.SIGTERM)
 
 	select {
-	case <-sigchan:
+	case sig := <-sigchan:
+		log.Infof("Received %s, shutting down", sig)
 		server.Kill()
+		// Unix sockets not cleaned up automatically
+		u, err := url.Parse(DefaultConfig.SyslogAddress)
+		if err == nil && u.Scheme == "unixgram" {
+			os.Remove(u.Path)
+		}
 	}
 
+}
+
+func setupListener(server *syslog.Server, address string) error {
+	u, err := url.Parse(address)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Starting syslog server on %s...", address)
+	switch u.Scheme {
+	case "tcp":
+		return server.ListenTCP(u.Host)
+	case "udp":
+		return server.ListenUDP(u.Host)
+	case "unixgram":
+		return server.ListenUnixgram(u.Path)
+	default:
+		return fmt.Errorf("Did not recognize syslog server scheme '%s'", u.Scheme)
+	}
 }
